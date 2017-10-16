2183796690fa7ad2cdb9f78f022c14beb425d12e
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-10-16 11:42:03 -0500

    4.R

---
+++ exercise4.R
@@ -0,0 +1,11 @@
+abs(-15.5)
+signif(4.483847,digits=2)
+round(3.8)
+tolower("species")
+toupper("species")
+
+apple<-sqrt(2.6)
+square<-(signif(apple ,digits = 2))
+print(square)
+
+(signif(sqrt(2.6), digits = 2))
