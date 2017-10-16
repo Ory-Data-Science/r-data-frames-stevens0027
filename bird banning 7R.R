ef56cfeb51a8e23a313c20121eb76bc4a38f6395
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-10-16 12:38:44 -0500

    7R

---
+++ bird banning 7R.R
@@ -0,0 +1,14 @@
+number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27, 
+                     36, 25, 9, 38, 21, 12, 122, 87, 36, 3, 0, 5, 55, 62, 98, 32, 
+                     900, 33, 14, 39, 56, 81, 29, 38, 1, 0, 143, 37, 98, 77, 92, 
+                     83, 34, 98, 40, 45, 51, 17, 22, 37, 48, 38, 91, 73, 54, 46,
+                     102, 273, 600, 10, 11)
+
+length(number_of_birds)
+print("83 birds were counted")
+sum(number_of_birds)ed
+print("11 birds were count")
+min(number_of_birds)
+max(number_of_birds)
+mean(number_of_birds)
+      
