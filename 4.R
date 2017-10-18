cf9d9f1f7a8f331bad5101b3a182615af6855ce2
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-10-18 10:24:10 -0500

    4.R

--- 4.R
+++ 4.R
@@ -1,11 +1,23 @@
 abs(-15.5)
+#[1] 15.5
+
 signif(4.483847,digits=2)
+#[1] 4.5
+
 round(3.8)
+#[1] 4
+
 tolower("species")
+#[1] "species"
+
 toupper("species")
+#[1] "SPECIES"
 
 apple<-sqrt(2.6)
 square<-(signif(apple ,digits = 2))
 print(square)
+#[1] 1.6
+
 
 (signif(sqrt(2.6), digits = 2))
+#[1] 1.6
