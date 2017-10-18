17a4b1ac9388d83202511c700f5129c62c7823d4
stevens0027 <31221878+stevens0027@users.noreply.github.com>
2017-10-18 10:26:27 -0500

    5.R

--- 5.R
+++ 5.R
@@ -5,7 +5,12 @@ site2_area_m2 <- 450
 site1_npp_day <- site1_g_carbon_m2_day * site1_area_m2
 site2_npp_day <- site2_g_carbon_m2_day * site2_area_m2
 print(site1_npp_day)
+#[1] 1000
 print(site2_npp_day)
+#[1] 1035
 print(site1_npp_day+site2_npp_day)
+#[1] 2035
 print(abs(site1_npp_day-site2_npp_day))
+#[1] 35
 print((site1_npp_day+site2_npp_day)*365)
+#[1] 742775
